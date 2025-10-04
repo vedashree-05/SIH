9c77357345eae0d4fe229597e967bf970d4c68fd
vedashree-05 <163523010+vedashree-05@users.noreply.github.com>
2025-10-04 13:46:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -156,7 +156,7 @@ function manageTraffic() {
 }
 
 function openPilgrimView() {
-    window.open('pilgrim-app', '_blank');
+    window.open('piligrim-app.html', '_blank');
 }
 
 // =============================================
